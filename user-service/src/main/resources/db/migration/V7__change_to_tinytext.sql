50a11cba8e7cf0d851d03f3afffbf7fad0079123
LittleLC <202003340112@zjut.edu.cn>
2025-10-16 12:22:52 +0800

    add user deploy

---
+++ user-service/src/main/resources/db/migration/V7__change_to_tinytext.sql
@@ -0,0 +1,5 @@
+ALTER TABLE profiles MODIFY COLUMN summary TINYTEXT;
+
+ALTER TABLE profile_educations MODIFY COLUMN description TINYTEXT;
+
+ALTER TABLE profile_experiences MODIFY COLUMN description TINYTEXT;
