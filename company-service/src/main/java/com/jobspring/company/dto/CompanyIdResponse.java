6a1ad92745cefbf24eea3b13ef67d038b6ddd292
XinxingHu280311516 <E1288029@u.nus.edu>
2025-10-18 14:48:52 +0800

    HR VIEW HIS COMPANY JOB

--- company-service/src/main/java/com/jobspring/company/dto/CompanyIdResponse.java
+++ company-service/src/main/java/com/jobspring/company/dto/CompanyIdResponse.java
@@ -4,9 +4,18 @@ package com.jobspring.company.dto;
 public class CompanyIdResponse {
     private Long companyId;
 
-    public CompanyIdResponse() {}
-    public CompanyIdResponse(Long companyId) { this.companyId = companyId; }
+    public CompanyIdResponse() {
+    }
 
-    public Long getCompanyId() { return companyId; }
-    public void setCompanyId(Long companyId) { this.companyId = companyId; }
+    public CompanyIdResponse(Long companyId) {
+        this.companyId = companyId;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
 }
